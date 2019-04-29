bf0878352dc00fd2331a69261b21f261065d4b12
nchristanto <christanto.nanda@gmail.com>
2019-04-30 03:39:43 +0700

    fix naming variable

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
-// import { LikeButton } from './components/LikeButton'
-
-// import logo from './logo.svg';
 import './App.css';
-
 import AppBar from './components/AppBar'
-
 import Jumbotron from './components/Jumbotron'
-
 import { NavigationBar } from './components/NavigationBar'
-
 import RecipeReviewCard from './components/RecipeReviewCard'
-
 import MusicPlayer from './components/MusicPlayer'
-
 import BottomNavigation from './components/BottomNavigation'
-
-
 
 function App() {
   return (
     <div class = "container">
     <AppBar />
-      <div class = "jumbotron">
+      <div className = "jumbotron">
       <Jumbotron />
       </div>
       <NavigationBar />
-        <div class = "card">
+        <div className = "card">
           <RecipeReviewCard />
           <RecipeReviewCard />
           <RecipeReviewCard />
